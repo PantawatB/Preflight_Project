59ffdf1c14cffdc34e588e9b383a2d15ab8793b5
Miracle <122048881+Miracles01@users.noreply.github.com>
2025-08-10 07:26:39 +0700

    *

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -8,9 +8,7 @@ import {
 
 export const todoTable = pgTable("todo", {
   id: uuid("id").primaryKey().defaultRandom(),
-  title: varchar("title", { length: 255 }).notNull(),
-  profile_url: varchar("profile_url", { length: 255 }),
-  status: varchar("status", { length: 50 }).default("pending").notNull(),
+  todoText: varchar("todo_text", { length: 255 }).notNull(),
   isDone: boolean("is_done").default(false),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
@@ -18,14 +16,14 @@ export const todoTable = pgTable("todo", {
   ),
 });
 
+
 export const ownerTable = pgTable("owner", {
   id: varchar("id", { length: 50 }).primaryKey(),
   Name: varchar("name", { length: 255 }).notNull(),
   course_id: varchar("course_id", { length: 20 }).notNull(),
-  section: varchar("section", { length: 20 }).notNull(),
+  section: varchar("section", { length: 20 }).notNull(), 
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
     () => new Date()
   ),
 });
-
