6363aa4aa8726188a95aaba08ea976966ce84d96
pantawatb <pantawat.korn@gmail.com>
2025-08-10 23:02:45 +0700

    fix

--- backend/db/schema.ts
+++ backend/db/schema.ts
@@ -16,14 +16,3 @@ export const todoTable = pgTable("todo", {
   ),
 });
 
-
-export const ownerTable = pgTable("owner", {
-  id: varchar("id", { length: 50 }).primaryKey(),
-  Name: varchar("name", { length: 255 }).notNull(),
-  course_id: varchar("course_id", { length: 20 }).notNull(),
-  section: varchar("section", { length: 20 }).notNull(), 
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at", { mode: "date", precision: 3 }).$onUpdate(
-    () => new Date()
-  ),
-});
